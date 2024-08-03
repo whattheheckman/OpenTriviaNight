cea6578f9f5f42f0403e0dbd4d49c9a4858f2772
BrownKnight <amanrk@hotmail.co.uk>
2024-08-03 18:39:33 +0100

    Improve header layout

--- ui/src/layout/Header.tsx
+++ ui/src/layout/Header.tsx
@@ -5,10 +5,15 @@ export default function Header() {
     const { game } = useContext(GameContext);
 
     return (
-        <div className="grid grid-cols-3 px-2 h-14 w-100 content-center items-center bg-orange-400 border-b-2 border-black">
-            <span></span>
-            <span className="flex justify-center font-bold text-xl">Open Trivia Night</span>
-            <span className="flex justify-end font-semibold">{game ? `Game ID: ${game.id}` : ""}</span>
+        <div className="flex justify-between px-4 h-14 w-100 items-center bg-orange-400 border-b-2 border-black">
+            <span className="font-bold text-xl shrink">Open Trivia Night</span>
+            {game
+                ? <div>
+                    <span>Game ID: </span>
+                    <span className="font-semibold italic">{game.id}</span>
+                </div>
+                : <div></div>}
+
         </div>
     )
 }
