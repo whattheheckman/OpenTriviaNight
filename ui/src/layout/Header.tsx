52d7aeecf6d1cd50a6cd86ec8d3981460b2baf52
BrownKnight <amanrk@hotmail.co.uk>
2024-08-04 19:51:19 +0100

    Show a hint tooltip to lead users to use the header dialog

--- ui/src/layout/Header.tsx
+++ ui/src/layout/Header.tsx
@@ -1,7 +1,6 @@
 import { useContext, useEffect, useState } from "react";
 import { GameContext } from "../GameContext";
-import { Modal } from "flowbite-react";
-import PlayerScoreTable from "../game/common/PlayerScoreTable";
+import { Modal, Popover } from "flowbite-react";
 import HeaderModal from "./HeaderModal";
 
 export default function Header() {
@@ -11,21 +10,30 @@ export default function Header() {
 
   useEffect(() => {
     if (game?.state.state === "WaitingToStart") {
-
+      setHintOpen(true)
     }
   }, [game?.state.state])
 
   return (
     <>
-      <div className="flex justify-between md:justify-center px-4 md:px-6 h-14 w-100 items-center bg-orange-400 border-b-2 border-black">
-        <button className="font-bold text-left md:text-center text-xl grow self-stretch" onClick={() => setModalOpen(true)}>Open Trivia Night</button>
-        {game
-          ? <div className="md:absolute right-0 md:pr-4 z-10">
-            <span>Game ID: </span>
-            <span className="font-semibold italic">{game.id}</span>
-          </div>
-          : <div></div>}
-      </div>
+      <Popover
+        aria-labelledby="area-popover"
+        open={hintOpen}
+        onOpenChange={() => setHintOpen(false)}
+        content={
+          <div className="w-72 p-4 bg-gray-100">Tap the top bar to see other players scores, and manage the game.</div>
+        }
+      >
+        <div className="flex justify-between md:justify-center px-4 md:px-6 h-14 w-100 items-center bg-orange-400 border-b-2 border-black">
+          <button className="font-bold text-left md:text-center text-xl grow self-stretch" onClick={() => setModalOpen(true)}>Open Trivia Night</button>
+          {game
+            ? <div className="md:absolute right-0 md:pr-4 z-10">
+              <span>Game ID: </span>
+              <span className="font-semibold italic">{game.id}</span>
+            </div>
+            : <div></div>}
+        </div>
+      </Popover>
 
       <Modal show={modalOpen} dismissible onClose={() => setModalOpen(false)} popup>
         <Modal.Header />
