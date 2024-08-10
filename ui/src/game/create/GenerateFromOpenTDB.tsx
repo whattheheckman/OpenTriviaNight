73b765cf04258b4b83ab55549e67047b2c2abdc0
BrownKnight <amanrk@hotmail.co.uk>
2024-08-10 19:21:57 +0100

    Format files

--- ui/src/game/create/GenerateFromOpenTDB.tsx
+++ ui/src/game/create/GenerateFromOpenTDB.tsx
@@ -6,113 +6,113 @@ import useApiClient from "../../useApiClient";
 
 type Props = {
   updateQuestions: (category: string, questions: Question[]) => void;
-}
+};
 
 // These categories are available at the API https://opentdb.com/api_category.php
 // Hardcoding these because constantly pinging the API for these seems like overkill
 const CATEGORIES = [
   {
     id: 9,
-    name: "General Knowledge"
+    name: "General Knowledge",
   },
   {
     id: 10,
-    name: "Entertainment: Books"
+    name: "Entertainment: Books",
   },
   {
     id: 11,
-    name: "Entertainment: Film"
+    name: "Entertainment: Film",
   },
   {
     id: 12,
-    name: "Entertainment: Music"
+    name: "Entertainment: Music",
   },
   {
     id: 13,
-    name: "Entertainment: Musicals & Theatres"
+    name: "Entertainment: Musicals & Theatres",
   },
   {
     id: 14,
-    name: "Entertainment: Television"
+    name: "Entertainment: Television",
   },
   {
     id: 15,
-    name: "Entertainment: Video Games"
+    name: "Entertainment: Video Games",
   },
   {
     id: 16,
-    name: "Entertainment: Board Games"
+    name: "Entertainment: Board Games",
   },
   {
     id: 17,
-    name: "Science & Nature"
+    name: "Science & Nature",
   },
   {
     id: 18,
-    name: "Science: Computers"
+    name: "Science: Computers",
   },
   {
     id: 19,
-    name: "Science: Mathematics"
+    name: "Science: Mathematics",
   },
   {
     id: 20,
-    name: "Mythology"
+    name: "Mythology",
   },
   {
     id: 21,
-    name: "Sports"
+    name: "Sports",
   },
   {
     id: 22,
-    name: "Geography"
+    name: "Geography",
   },
   {
     id: 23,
-    name: "History"
+    name: "History",
   },
   {
     id: 24,
-    name: "Politics"
+    name: "Politics",
   },
   {
     id: 25,
-    name: "Art"
+    name: "Art",
   },
   {
     id: 26,
-    name: "Celebrities"
+    name: "Celebrities",
   },
   {
     id: 27,
-    name: "Animals"
+    name: "Animals",
   },
   {
     id: 28,
-    name: "Vehicles"
+    name: "Vehicles",
   },
   {
     id: 29,
-    name: "Entertainment: Comics"
+    name: "Entertainment: Comics",
   },
   {
     id: 30,
-    name: "Science: Gadgets"
+    name: "Science: Gadgets",
   },
   {
     id: 31,
-    name: "Entertainment: Japanese Anime & Manga"
+    name: "Entertainment: Japanese Anime & Manga",
   },
   {
     id: 32,
-    name: "Entertainment: Cartoon & Animations"
-  }
-]
+    name: "Entertainment: Cartoon & Animations",
+  },
+];
 
 type OpenTDBQuestionResponse = {
-  response_code: number,
-  results: { question: string, correct_answer: string }[]
-}
+  response_code: number;
+  results: { question: string; correct_answer: string }[];
+};
 
 export default function GenerateFromOpenTDB({ updateQuestions }: Props) {
   const [selectedCategory, setSelectedCategory] = useState<number>(9);
@@ -121,38 +121,40 @@ export default function GenerateFromOpenTDB({ updateQuestions }: Props) {
   const apiClient = useApiClient();
 
   const generateQuestions = () => {
-    apiClient
-      .getQuestionsFromOpenTDB({ category: selectedCategory, difficulty: difficulty })
-      ?.then((res: OpenTDBQuestionResponse) => {
-        const questions: Question[] = res.results.map((r, count) => {
-          return {
-            questionId: crypto.randomUUID(),
-            detail: decodeURIComponent(r.question),
-            correctAnswer: decodeURIComponent(r.correct_answer),
-            value: (count + 1) * 100,
-            answered: false
-          }
-        });
-        const category = CATEGORIES.find(x => x.id == selectedCategory);
-        updateQuestions(category?.name ?? "Unknown", questions);
-      })
-  }
+    apiClient.getQuestionsFromOpenTDB({ category: selectedCategory, difficulty: difficulty })?.then((res: OpenTDBQuestionResponse) => {
+      const questions: Question[] = res.results.map((r, count) => {
+        return {
+          questionId: crypto.randomUUID(),
+          detail: decodeURIComponent(r.question),
+          correctAnswer: decodeURIComponent(r.correct_answer),
+          value: (count + 1) * 100,
+          answered: false,
+        };
+      });
+      const category = CATEGORIES.find((x) => x.id == selectedCategory);
+      updateQuestions(category?.name ?? "Unknown", questions);
+    });
+  };
 
   return (
     <div className="flex flex-col">
       <div className="flex flex-col sm:flex-row gap-2">
         <div className="flex-1 grow">
           <Label className="mt-1" htmlFor="categories" value="Select Category" />
-          <Select id="categories" required value={selectedCategory} onChange={e => setSelectedCategory(parseInt(e.target.value))}>
-            {CATEGORIES.map(c => {
-              return <option key={c.id} value={c.id}>{c.name}</option>
+          <Select id="categories" required value={selectedCategory} onChange={(e) => setSelectedCategory(parseInt(e.target.value))}>
+            {CATEGORIES.map((c) => {
+              return (
+                <option key={c.id} value={c.id}>
+                  {c.name}
+                </option>
+              );
             })}
           </Select>
         </div>
 
         <div className="flex-1 grow">
           <Label className="mt-2" htmlFor="difficulty" value="Select Difficulty" />
-          <Select id="difficulty" required value={difficulty} onChange={e => setDifficulty(e.target.value)}>
+          <Select id="difficulty" required value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
@@ -160,8 +162,10 @@ export default function GenerateFromOpenTDB({ updateQuestions }: Props) {
         </div>
       </div>
 
-      <Button className="mt-4" gradientDuoTone="pinkToOrange" onClick={generateQuestions}><HiOutlineRefresh className="h-5 mr-2" />Generate Questions</Button>
-
+      <Button className="mt-4" gradientDuoTone="pinkToOrange" onClick={generateQuestions}>
+        <HiOutlineRefresh className="h-5 mr-2" />
+        Generate Questions
+      </Button>
     </div>
-  )
+  );
 }
--- ui/src/game/create/GenerateFromTriviaApi.tsx
+++ ui/src/game/create/GenerateFromTriviaApi.tsx
@@ -6,55 +6,55 @@ import useApiClient from "../../useApiClient";
 
 type Props = {
   updateQuestions: (category: string, questions: Question[]) => void;
-}
+};
 
 type TriviaQuestionResponse = {
-  correctAnswer: string,
-  question: { text: string }
-}
+  correctAnswer: string;
+  question: { text: string };
+};
 
-const CATEGORIES: { id: string, name: string }[] = [
+const CATEGORIES: { id: string; name: string }[] = [
   {
     id: "music",
-    name: "Music"
+    name: "Music",
   },
   {
     id: "sport_and_leisure",
-    name: "Sport & Leisure"
+    name: "Sport & Leisure",
   },
   {
     id: "film_and_tv",
-    name: "Film & TV"
+    name: "Film & TV",
   },
   {
     id: "arts_and_literature",
-    name: "Arts & Literature"
+    name: "Arts & Literature",
   },
   {
     id: "history",
-    name: "History"
+    name: "History",
   },
   {
     id: "society_and_culture",
-    name: "Society & Culture"
+    name: "Society & Culture",
   },
   {
     id: "science",
-    name: "Science"
+    name: "Science",
   },
   {
     id: "geography",
-    name: "Geography"
+    name: "Geography",
   },
   {
     id: "food_and_drink",
-    name: "Food & Drink"
+    name: "Food & Drink",
   },
   {
     id: "general_knowledge",
-    name: "General Knowledge"
-  }
-]
+    name: "General Knowledge",
+  },
+];
 
 export default function GenerateFromTriviaApi({ updateQuestions }: Props) {
   const [selectedCategory, setSelectedCategory] = useState<string>("general_knowledge");
@@ -63,38 +63,40 @@ export default function GenerateFromTriviaApi({ updateQuestions }: Props) {
 
   const generateQuestions = () => {
     // TODO: Support tags for more fine grained categories
-    apiClient
-      .getQuestionsFromTriviaApi(selectedCategory, difficulty)
-      ?.then((res: TriviaQuestionResponse[]) => {
-        let questions: Question[] = res.map((r, count) => {
-          return {
-            questionId: crypto.randomUUID(),
-            detail: r.question.text,
-            correctAnswer: r.correctAnswer,
-            value: (count + 1) * 100,
-            answered: false
-          }
-        });
-        let category = CATEGORIES.find(x => x.id == selectedCategory)
-        updateQuestions(category?.name ?? "Unknown", questions);
-      })
-  }
+    apiClient.getQuestionsFromTriviaApi(selectedCategory, difficulty)?.then((res: TriviaQuestionResponse[]) => {
+      const questions: Question[] = res.map((r, count) => {
+        return {
+          questionId: crypto.randomUUID(),
+          detail: r.question.text,
+          correctAnswer: r.correctAnswer,
+          value: (count + 1) * 100,
+          answered: false,
+        };
+      });
+      const category = CATEGORIES.find((x) => x.id == selectedCategory);
+      updateQuestions(category?.name ?? "Unknown", questions);
+    });
+  };
 
   return (
     <div className="flex flex-col">
       <div className="flex flex-col sm:flex-row gap-2">
         <div className="flex-1 grow">
           <Label className="mt-1" htmlFor="categories" value="Select Category" />
-          <Select id="categories" required value={selectedCategory} onChange={e => setSelectedCategory(e.target.value)}>
-            {CATEGORIES.map(c => {
-              return <option key={c.id} value={c.id}>{c.name}</option>
+          <Select id="categories" required value={selectedCategory} onChange={(e) => setSelectedCategory(e.target.value)}>
+            {CATEGORIES.map((c) => {
+              return (
+                <option key={c.id} value={c.id}>
+                  {c.name}
+                </option>
+              );
             })}
           </Select>
         </div>
 
         <div className="flex-1 grow">
           <Label className="mt-2" htmlFor="difficulty" value="Select Difficulty" />
-          <Select id="difficulty" required value={difficulty} onChange={e => setDifficulty(e.target.value)}>
+          <Select id="difficulty" required value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
@@ -102,8 +104,10 @@ export default function GenerateFromTriviaApi({ updateQuestions }: Props) {
         </div>
       </div>
 
-      <Button className="mt-4" gradientDuoTone="pinkToOrange" onClick={generateQuestions}><HiOutlineRefresh className="h-5 mr-2" />Generate Questions</Button>
-
+      <Button className="mt-4" gradientDuoTone="pinkToOrange" onClick={generateQuestions}>
+        <HiOutlineRefresh className="h-5 mr-2" />
+        Generate Questions
+      </Button>
     </div>
-  )
+  );
 }
