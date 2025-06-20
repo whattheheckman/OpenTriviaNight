3ed2604b1d26f6be638e8906fb297a2d656d4f19
whattheheckman <40683851+whattheheckman@users.noreply.github.com>
2025-06-20 12:07:54 -0700

    qr code on waiting to start screen

--- ui/src/game/WaitingToStartScreen.tsx
+++ ui/src/game/WaitingToStartScreen.tsx
@@ -3,23 +3,27 @@ import { GameContext } from "../GameContext";
 import { Button, Spinner, Table, TableBody, TableCell, TableHead, TableHeadCell, TableRow } from "flowbite-react";
 import useApiClient from "../useApiClient";
 import GameIdCopyButton from "./common/GameIdCopyButton";
+import QRCode from "react-qr-code";
 
 export default function WaitingToStartScreen() {
-  const { game, username } = useContext(GameContext);
+  const { game, username, prefs } = useContext(GameContext);
   const apiClient = useApiClient();
 
   if (!game) {
     return <></>;
   }
 
-  const isHost = game.players.find((x) => x.username === username)?.role === "Host";
+  const isHost =
+    game.players.find((x) => x.username === username)?.role === "Host";
 
   return (
     <div className="flex flex-col max-w-(--breakpoint-md) mx-auto mb-8 items-stretch">
       <div className="text-center my-4">
         <Spinner />
       </div>
-      <h1 className="text-lg font-semibold self-center mb-5">Waiting for Host to Start Game</h1>
+      <h1 className="text-lg font-semibold self-center mb-5">
+        Waiting for Host to Start Game
+      </h1>
 
       <Table striped>
         <TableHead>
@@ -45,6 +49,16 @@ export default function WaitingToStartScreen() {
                 <span className="py-2 pr-1">Use Game ID </span>
                 <GameIdCopyButton className="text-gray-500 hover:bg-gray-100 active:bg-gray-200" />
                 <span className="py-2 pl-1">to join</span>
+              </div>
+              <div>
+                {!prefs.hideGameId ? (
+                  <QRCode
+                    style={{ height: "auto", maxWidth: "100%" }}
+                    value={window.location.href + "?gameId=" + game.id}
+                  />
+                ) : (
+                  <></>
+                )}
               </div>
             </TableCell>
           </TableRow>
